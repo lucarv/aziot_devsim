200c10e4ac2dbd46328a59f99b2eb72afa4c94fd
lucarv <lucarv@microsoft.com>
2019-09-22 17:43:19 +0200

    added module to send data to azure iot hub

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var Message = require('azure-iot-device').Message;
 
 // String containing Hostname, Device Id & Device Key in the following formats:
 //  "HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
-var connectionString = '<YOUR CONNECTION STRING>'
+var connectionString = process.env.cs;
 
 // fromConnectionString must specify a transport constructor, coming from any transport package.
 var client = Client.fromConnectionString(connectionString, Protocol);
@@ -38,18 +38,17 @@ const telemetry = () => {
   /**/
   let message = new Message(JSON.stringify(payload));
 
-// A message can have custom properties that are also encoded and can be used for routing
-message.properties.add('deviceType', 'test');
+  // A message can have custom properties that are also encoded and can be used for routing
+  message.properties.add('deviceType', 'test');
 
-// A unique identifier can be set to easily track the message in your application
-message.messageId = uuid.v4();
+  // A unique identifier can be set to easily track the message in your application
+  message.messageId = uuid.v4();
   client.sendEvent(message, function (err) {
     if (err) {
       console.error('Could not send: ' + err.toString());
       process.exit(-1);
     } else {
-      console.log('Message sent\n '+ 'Current Sensor Temperature is: ' + temperature);
-      temperature += 0.2
+      temperature += 0.5
     }
   });
 };
@@ -60,13 +59,13 @@ client.open(function (err) {
   } else {
     console.log('Client connected');
     // Create device Twin
-    client.getTwin(function(err, twin) {
+    client.getTwin(function (err, twin) {
       if (err) {
         console.error('could not get twin');
         process.exit(-1);
       } else {
         console.log('twin created');
-        twin.on('properties.desired', function(delta) {
+        twin.on('properties.desired', function (delta) {
           console.log('new desired properties received:');
           console.log(JSON.stringify(delta));
           if (delta.hasOwnProperty('telemetry')) {
@@ -78,7 +77,7 @@ client.open(function (err) {
               clearInterval(tele)
             }
           }
-      });
+        });
       }
     });
 
@@ -87,22 +86,42 @@ client.open(function (err) {
       process.exit(-1);
     });
 
-        // register handler for 'reset'
-        client.onDeviceMethod('reset', function (request, response) {
-          console.log('received a request for reset');
-          temperature = 25
-          var responsePayload = {
-            result: 'sensor reset'
-          };
-    
-          response.send(200, responsePayload, function (err) {
-            if (err) {
-              console.error('Unable to send method response: ' + err.toString());
-              process.exit(-1);
-            } else {
-              console.log('response to reset sent.');
-            }
-          });
-        });
+    client.on('message', function (msg) {
+      console.log('-------------------------------');
+      console.log(' ' + msg.data.toString());
+      console.log('-------------------------------');
+
+      client.complete(msg, function (err) {
+        if (err) {
+          client.error('could not settle message: ' + err.toString());
+          process.exit(-1);
+        } else {
+          console.log('message successfully accepted');
+        }
+      });
+      // The AMQP and HTTP transports also have the notion of completing, rejecting or abandoning the message.
+      // When completing a message, the service that sent the C2D message is notified that the message has been processed.
+      // When rejecting a message, the service that sent the C2D message is notified that the message won't be processed by the device. the method to use is client.reject(msg, callback).
+      // When abandoning the message, IoT Hub will immediately try to resend it. The method to use is client.abandon(msg, callback).
+      // MQTT is simpler: it accepts the message by default, and doesn't support rejecting or abandoning a message.
+    });
+
+    // register handler for 'reset'
+    client.onDeviceMethod('reset', function (request, response) {
+      console.log('received a request for reset');
+      temperature = 25
+      var responsePayload = {
+        result: 'sensor reset'
+      };
+
+      response.send(200, responsePayload, function (err) {
+        if (err) {
+          console.error('Unable to send method response: ' + err.toString());
+          process.exit(-1);
+        } else {
+          console.log('response to reset sent.');
+        }
+      });
+    });
   }
 });
