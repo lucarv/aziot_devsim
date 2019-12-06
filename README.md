fc54c07ffb344db0826ac5443e34210a5f184656
lucarv <lucarv@microsoft.com>
2019-12-06 09:29:29 +0100

    added sample for constrained device

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-## node.js sample: Device simulator for IoT Hub Client
+# Device simulator for IoT Hub Client
 
-How to run this sample:
+## How to run this sample:
+
+Note: if you are running this on a raspberry pi, nake sure you have node.js v10 or higher installed  
 
 1. clone this repo
 ```
@@ -12,8 +14,12 @@ How to run this sample:
 > npm install
 ```
 3. edit the index.js script and enter your device connection string, that copied when you created the device.
-4. run the sample
+4. run the sample for a normal device from the command line
 ```
-> node index
+> node index 
 ```
-5. Return to the LAB 1 instructions
+5. run the sample for a constrained device from the command line
+```
+> node index 
+```
+6. Note eventual text outputted to the console.
---
+++ constrained.js
@@ -0,0 +1,61 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+'use strict';
+require('dotenv').config()
+var uuid = require('uuid');
+
+var Protocol = require('azure-iot-device-mqtt').Mqtt;
+var Client = require('azure-iot-device').Client;
+var Message = require('azure-iot-device').Message;
+
+// String containing Hostname, Device Id & Device Key in the following formats:
+//  "HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
+var connectionString = process.env.cs;
+var deviceId = connectionString.substring((connectionString.indexOf(';Device') + 10), connectionString.indexOf(';SharedAccess'))
+console.log(deviceId);
+
+// fromConnectionString must specify a transport constructor, coming from any transport package.
+var client = Client.fromConnectionString(connectionString, Protocol);
+
+var counter = 0;
+
+const run = () => {
+  counter++;
+  client.open(function (err) {
+    if (err) {
+      console.error('Could not connect: ' + err.message);
+    } else {
+      // connect
+      console.log(`${new Date()}: ${deviceId} connected`);
+      let payload = {
+        counter,
+        "messageId": uuid.v4()
+      }
+      let message = new Message(JSON.stringify(payload));
+      message.properties.add('tenant', 'electrolux');
+      // send telemetry
+      client.sendEvent(message, function (err) {
+        if (err) {
+          console.error('Could not send: ' + err.toString());
+          process.exit(-1);
+        } else {
+          console.log('sent message #' + counter);
+          // wait 5 seconds then disconnect
+          setTimeout(function () {
+            client.close(function (err) {
+              if (err) {
+                console.error('Unable to close the connection: ' + err.toString());
+              } else {
+                console.log(`${new Date()}: ${deviceId} disconnected`);
+              }
+            });
+          }, process.env.wait);
+        }
+      });
+    }
+  });
+};
+
+console.log(process.env.FREQ)
+setInterval(run, 2000);
